0e5ce5c62b103c935a10a595595a86e668860b18
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-14 09:24:43 +0200

    Added statements to each statement for the clown instances.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -31,20 +31,24 @@ namespace ConsoleApp
             }
             //----------------------------------------------------------------------------------//
             // From Chapter 3 (Sharpen Your Pencil Exercise) - An instance uses fields to keep track of things
+            // These statements create an instance of Clown and then set its fields:
             Clown oneClown = new Clown();
             oneClown.Name = "Boffo";
             oneClown.Height = 14;
             oneClown.TalkAboutYourself();
 
+            //These statements instantiate a second Clown object and fill it with data:
             Clown anotherClown = new Clown();
             anotherClown.Name = "Biff";
             anotherClown.Height = 16;
 
+            // Now we instantiate a third Clown object and use data from the other two instances to set its fields:
             Clown clown3 = new Clown();
             clown3.Name = anotherClown.Name;
             clown3.Height = oneClown.Height - 3;
             clown3.TalkAboutYourself();
 
+            // Notic how there's no "new" statement here -- we're not creating a new object, just modifying one already in memory:
             anotherClown.Height *= 2;
             anotherClown.TalkAboutYourself();
         }
