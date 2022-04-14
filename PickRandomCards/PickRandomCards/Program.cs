00c8156d6fb5b086761f3ca9ece3393b14946490
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-14 08:53:47 +0200

    added some more comments about the static keyword in the program.cs class

--- PickRandomCards/PickRandomCards/Program.cs
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -12,9 +12,13 @@ namespace PickRandomCards
             if (int.TryParse(line, out int numberOfCards))
             {
                 // this block is executed if line COULD be converted to an int
-                // value that's stored in a new variable called numberOfCards
+                // value that's stored in a new variable called numberOfCards.
 
-                // I can just call the method like this - CardPicker.PickSomeCards(numberOfCards);
+                // I would have to do this if I did not use the static keyword(in the PickSomeCards method):
+                // CardPicker results = new CardPicker();
+                // then I would have to access the method by using the results variable - results.PickSomeCards(numberOfCards).
+
+                // Using the static keyword allows me to just call the method like this - CardPicker.PickSomeCards(numberOfCards);
                 string[] results = CardPicker.PickSomeCards(numberOfCards);
 
                 foreach (string card in results)
