4b17c4a9dab9663033e95e93a38d575af3115213
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-15 09:07:01 +0200

    add guy class

--- ConsoleAppBettingGame/ConsoleAppBettingGame/Program.cs
+++ ConsoleAppBettingGame/ConsoleAppBettingGame/Program.cs
@@ -6,69 +6,39 @@ namespace ConsoleAppBettingGame
     {
         static void Main(string[] args)
         {
+            double odds = .75;
             Random random = new Random();
-            double odds = 0.75;
-
-            Better billy = new Better { Name = "Billy", Cash = 100 };
-            Better bob = new Better { Name = "Bob", Cash = 100 };
-
-            Console.WriteLine($"Welcome to the casino. The odds are {odds}");
-            while (billy.Cash > 0 && bob.Cash > 0)
+            Guy player = new Guy() { Cash = 100, Name = "The player" };
+            Console.WriteLine("Welcome to the casino. The odds are " + odds);
+            while (player.Cash > 0)
             {
-                billy.WriteMyInfo();
-                bob.WriteMyInfo();
-                Console.Write("Who is betting: ");
-                string better = Console.ReadLine();
+                player.WriteMyInfo();
                 Console.Write("How much do you want to bet: ");
                 string howMuch = Console.ReadLine();
-
                 if (int.TryParse(howMuch, out int amount))
                 {
-                    if (better == "Billy")
+                    int pot = player.GiveCash(amount) * 2;
+                    if (pot > 0)
                     {
-                        int pot = billy.GiveCash(amount) * 2;
-
-                        if (pot > 0)
+                        if (random.NextDouble() > odds)
                         {
-                            if (random.NextDouble() > odds)
-                            {
-                                int winnings = pot;
-                                Console.WriteLine($"You win {winnings}");
-                                billy.ReceiveCash(winnings);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Bad luck, you lose.");
-                            }
+                            int winnings = pot;
+                            Console.WriteLine("You win " + winnings);
+                            player.ReceiveCash(winnings);
                         }
-
-                    }
-                    if (better == "Bob")
-                    {
-                        int pot = bob.GiveCash(amount) * 2;
-
-                        if (pot > 0)
+                        else
                         {
-                            if (random.NextDouble() > odds)
-                            {
-                                int winnings = pot;
-                                Console.WriteLine($"You win {winnings}");
-                                bob.ReceiveCash(winnings);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Bad luck, you lose.");
-                            }
+                            Console.WriteLine("Bad luck, you lose.");
                         }
-
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Please enter a valid number");
+                    Console.WriteLine("Please enter a valid number.");
                 }
             }
-            Console.WriteLine("The house always wins!");
+            Console.WriteLine("The house always wins.");
         }
     }
+
 }
