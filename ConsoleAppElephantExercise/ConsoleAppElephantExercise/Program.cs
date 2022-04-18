bec63df0e3c612d9e636fdcad3fe254b3958b3d0
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-18 10:44:39 +0200

    game is finished

--- ConsoleAppElephantExercise/ConsoleAppElephantExercise/Program.cs
+++ ConsoleAppElephantExercise/ConsoleAppElephantExercise/Program.cs
@@ -6,6 +6,38 @@ namespace ConsoleAppElephantExercise
     {
         static void Main(string[] args)
         {
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            while (true)
+            {
+                char keyChar = Console.ReadKey(true).KeyChar;
+                Console.WriteLine($"You pressed {keyChar}");
+                if (keyChar == '1')
+                {
+                    Console.WriteLine("Calling lloyd.WhoAmI()");
+                    lloyd.WhoAmI();
+                }
+                else if (keyChar == '2')
+                {
+                    Console.WriteLine("Calling lucinda.WhoAmI()");
+                    lucinda.WhoAmI();
+                }
+                else if (keyChar == '3')
+                {
+                    Elephant holder;
+                    Console.WriteLine("References have been swapped");
+                    Console.WriteLine();
+                    holder = lloyd;
+                    lloyd = lucinda;
+                    lucinda = holder;
+                }
+                else
+                {
+                    return;
+                }
+            }
 
         }
     }
