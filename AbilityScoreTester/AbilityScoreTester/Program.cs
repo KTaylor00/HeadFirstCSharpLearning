72a69062911779c15caeec65048764c227e9f576
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-18 07:51:29 +0200

    Added 2 new methods, ReadDouble and ReadInt

--- AbilityScoreTester/AbilityScoreTester/Program.cs
+++ AbilityScoreTester/AbilityScoreTester/Program.cs
@@ -21,5 +21,45 @@ namespace AbilityScoreTester
                     return;
             }
         }
+
+        /// <summary>
+        /// Writes a prompt and read a double value from the console.
+        /// </summary>
+        /// <param name="lastUsedValue">The default value</param>
+        /// <param name="prompt">Prompt to print to the console.</param>
+        /// <returns>The double value read, or the default value if unable to parse</returns>
+        private static int ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write($"{prompt} [{lastUsedValue}]: ");
+            string value = Console.ReadLine();
+            if (double.TryParse(value, out lastUsedValue))
+            {
+                Console.WriteLine($"  using value" + value);
+            }
+            else
+            {
+                Console.WriteLine($"  using default value" + lastUsedValue);
+            }
+        }
+
+        /// <summary>
+        /// Writes a prompt and read an int value from the console.
+        /// </summary>
+        /// <param name="lastUsedValue">The default value</param>
+        /// <param name="prompt">Prompt to print to the console.</param>
+        /// <returns>The int value read, or the default value if unable to parse</returns>
+        private static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write($"{prompt} [{lastUsedValue}]: ");
+            string value = Console.ReadLine();
+            if (int.TryParse(value, out lastUsedValue))
+            {
+                Console.WriteLine($"  using value" + value);
+            }
+            else
+            {
+                Console.WriteLine($"  using default value" + lastUsedValue);
+            }
+        }
     }
 }
