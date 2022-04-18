d1bff127b1042f4db3852f53fbbaf97fae464de3
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-18 10:00:15 +0200

    initial commit

---
+++ ConsoleAppElephantExercise/ConsoleAppElephantExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppElephantExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
