2989b5c89cb16fb93921e5586dc04bee69477601
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-19 09:28:40 +0200

    initial commit. Add class called MenuItem. Create XAML layout window. Make a method called MakeTheMenu in MainWindow class.

---
+++ SloppyJoe/SloppyJoe/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SloppyJoe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            string guacamolePrice;
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >= 3)
+                {
+                    menuItems[i].Breads = new string[]
+                    {
+                        "plain bagel", "onion bagel", "pumpernickel bagel", "everything bagel"
+                    };
+                }
+            }
+        }
+    }
+}
