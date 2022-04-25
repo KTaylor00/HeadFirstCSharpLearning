6a3ee0860c847787037923f90052d8e2f8682d70
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-25 09:57:46 +0200

    Blazor version of the game is working

---
+++ PickACardBlazor/PickACardBlazor/CardPicker.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardBlazor
+{
+    internal class CardPicker
+    {
+        // When a field is static there’s only one copy of it, and it’s shared by all instances.
+        // So if you created multiple instances of CardPicker, they would all share the same random
+        // field.
+        /// <summary>
+        /// Generates random numbers to be used by CardPicker methods
+        /// </summary>
+        static Random random = new Random();
+
+        // When you use the static keyword to declare a field or method in a class, you don’t need 
+        // an instance(using the new keyword) of that class to access it. 
+        // Static methods are just like object 
+        // methods: they can take arguments, they can return values, and they live in classes.
+        /// <summary>
+        /// Picks a number of cards and returns them.
+        /// </summary>
+        /// <param name="numberOfCards">The number of cards to pick.</param>
+        /// <returns>An array of strings that contain the card names.</returns>
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            // get a random number from 1 to 4
+            int value = random.Next(1, 5);
+            // if it's 1 return the string Spades
+            if (value == 1)
+                return "Spades";
+            // if it's 2 return the string Hearts
+            if (value == 2)
+                return "Hearts";
+            // if it's 3 return the string Clubs
+            if (value == 3)
+                return "Clubs";
+            // if we haven't returned yet, return the string Diamonds
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            // get a random number from 1 to 14
+            int value = random.Next(1, 14);
+            // if it's 1 return string Ace
+            if (value == 1)
+                return "Ace";
+            // if it's 11 return string Jack
+            if (value == 11)
+                return "Jack";
+            // if it's 12 return string Queen
+            if (value == 12)
+                return "Queen";
+            // if it's 13 return string King
+            if (value == 13)
+                return "King";
+            // convert to string because value is an int type and the method is of type string.
+            return value.ToString();
+        }
+    }
+}
