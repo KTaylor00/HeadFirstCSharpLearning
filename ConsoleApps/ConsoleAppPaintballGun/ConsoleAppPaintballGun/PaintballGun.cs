49fe94c871819fdd83ed7d6b51fc75b8a03a84b3
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-30 09:37:47 +0200

    change the methods that were allowing the main method to have access to the private fields to properties

---
+++ ConsoleApps/ConsoleAppPaintballGun/ConsoleAppPaintballGun/PaintballGun.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaintballGun
+{
+    internal class PaintballGun
+    {
+        public const int MAGAZINE_SIZE = 16;
+
+        private int balls;
+        public int BallsLoaded { get; private set; }
+
+        public bool IsEmpty() => BallsLoaded == 0;
+
+        public int Balls
+        {
+            get { return balls; }
+
+            set
+            {
+                if (value > 0)
+                    balls = value;
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls < MAGAZINE_SIZE)
+                BallsLoaded = MAGAZINE_SIZE;
+            else
+                BallsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0 || balls == 0)
+                return false;
+
+            BallsLoaded--;
+            balls--;
+
+            return true;
+        }
+    }
+}
