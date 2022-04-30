2a3d63f0d2528a8ebc5e982dfdf92345f8be6225
Kieran Taylor <kmtaylor00@outlook.com>
2022-04-30 07:52:03 +0200

    game now works well.

---
+++ ConsoleApps/ConsoleAppHiLo/ConsoleAppHiLo/HiLoGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHiLo
+{
+    static class HiLoGame
+    {
+        // All constants are static.
+        public const int MAXIMUM = 10;
+        public static Random Random = new Random();
+        private static int currentNumber = Random.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+        // The main method can use the GetPot method to het its value without having a way to modify it.
+        public static int GetPot() => pot;
+
+        public static void Guess(bool higher)
+        {
+            int nextRandom = Random.Next(1, MAXIMUM + 1);
+
+            if (higher && nextRandom >= currentNumber || !higher && nextRandom <= currentNumber)
+            {
+                Console.WriteLine("You guessed right!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed wrong...");
+                pot--;
+            }
+
+            currentNumber = nextRandom;
+            Console.WriteLine($"The current number is {currentNumber}\n");
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (currentNumber >= half)
+                Console.WriteLine($"The number is at least {half}");
+            else
+                Console.WriteLine($"The number is at most {half}");
+
+            pot--;
+        }
+    }
+}
--- ConsoleApps/ConsoleAppHiLo/ConsoleAppHiLo/Program.cs
+++ ConsoleApps/ConsoleAppHiLo/ConsoleAppHiLo/Program.cs
@@ -6,7 +6,32 @@ namespace ConsoleAppHiLo
     {
         static void Main(string[] args)
         {
+            // Every instance of Random initialized with the same seed will generate the dame squence of pseudo-random numbers.
+            //HiLoGame.Random = new Random(1);
+            //Random seededRandom = new Random(1);
+            //Console.Write("The first 11 numbers will be: ");
+            //for (int i = 0; i < 10; i++)
+            //    Console.Write($"{seededRandom.Next(1, HiLoGame.MAXIMUM + 1)}");
 
+
+            Console.WriteLine("Welcome to HiLo.");
+            Console.WriteLine($"Guess numbers between 1 and {HiLoGame.MAXIMUM}.");
+
+            while (HiLoGame.GetPot() > 0)
+            {
+                Console.WriteLine("Press H for higher, L for lower, ? to buy a hint,");
+                Console.WriteLine($"or any other key to quit with.\n");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == 'h')
+                    HiLoGame.Guess(true);
+                else if (key == 'l')
+                    HiLoGame.Guess(false);
+                else if (key == '?')
+                    HiLoGame.Hint();
+                else
+                    return;
+            }
+            Console.WriteLine("The pot is empty. Bye!");
         }
     }
 }
