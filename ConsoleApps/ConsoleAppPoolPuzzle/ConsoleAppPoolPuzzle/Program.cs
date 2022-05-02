608831a72faee7ffb42bf8098f6f9ee4f83abe9e
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-02 07:36:44 +0200

    initial commit

---
+++ ConsoleApps/ConsoleAppPoolPuzzle/ConsoleAppPoolPuzzle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppPoolPuzzle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
