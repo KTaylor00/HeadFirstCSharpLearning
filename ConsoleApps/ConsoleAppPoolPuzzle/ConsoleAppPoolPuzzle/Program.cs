db5f8dfa7e0112782801a5c0540121d68ca7e6db
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-02 07:52:20 +0200

    Game has been made

--- ConsoleApps/ConsoleAppPoolPuzzle/ConsoleAppPoolPuzzle/Program.cs
+++ ConsoleApps/ConsoleAppPoolPuzzle/ConsoleAppPoolPuzzle/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleAppPoolPuzzle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Q q = new Q(Q.Random.Next(2) == 1);
+            while (true)
+            {
+                Console.Write($"{q.N1} {q.Op} {q.N2} = ");
+                if (!int.TryParse(Console.ReadLine(), out var i))
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    return;
+                }
+                if (q.Check(i))
+                {
+                    Console.WriteLine("Right!");
+                    q = new Q(Q.Random.Next(2) == 1);
+                }
+                else
+                    Console.WriteLine("Wrong! Try again.");
+            }
         }
     }
 }
