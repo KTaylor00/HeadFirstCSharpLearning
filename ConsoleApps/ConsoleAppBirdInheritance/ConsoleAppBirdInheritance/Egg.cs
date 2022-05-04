3fb38279d7296cd20300b7a21163537bc620bf1c
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-04 12:09:03 +0200

    initial commit, classes used to show inheritance

---
+++ ConsoleApps/ConsoleAppBirdInheritance/ConsoleAppBirdInheritance/Egg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBirdInheritance
+{
+    internal class Egg
+    {
+        public double Size { get; private set; }
+        public string Colour { get; private set; }
+
+        public Egg(double size, string colour)
+        {
+            Size = size;
+            Colour = colour;
+        }
+
+        public string Description
+        {
+            get { return $"A {Size:0.0}cm {Colour} egg\n"; }
+        }
+    }
+}
---
+++ ConsoleApps/ConsoleAppBirdInheritance/ConsoleAppBirdInheritance/Ostrich.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBirdInheritance
+{
+    internal class Ostrich : Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            Egg[] eggs = new Egg[numberOfEggs];
+            for (int i = 0; i < numberOfEggs; i++)
+            {
+                eggs[i] = new Egg(Bird.Random.NextDouble() + 12, "speckled");
+            }
+            return eggs;
+        }
+    }
+}
