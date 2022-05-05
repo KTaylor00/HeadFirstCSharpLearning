fd6163d6bbe9d9b548715384401107b3821cbde6
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-05 06:13:47 +0200

    initial commit, for inheritance learning

---
+++ ConsoleApps/ConsoleAppSafeInheritance/ConsoleAppSafeInheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleAppSafeInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SafeOwner owner = new SafeOwner();
+            Safe safe = new Safe();
+            JewelThief thief = new JewelThief();
+            thief.OpenSafe(safe, owner);
+            Console.ReadKey(true);
+        }
+    }
+}
