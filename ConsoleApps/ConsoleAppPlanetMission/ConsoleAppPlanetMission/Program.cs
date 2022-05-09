8dcf997523d29e1424dcdb279aff40ad11c2e2a8
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-09 08:09:55 +0200

    using abstract

---
+++ ConsoleApps/ConsoleAppPlanetMission/ConsoleAppPlanetMission/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppPlanetMission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Venus().MissionInfo());
+            Console.WriteLine(new Mars().MissionInfo());
+        }
+    }
+}
