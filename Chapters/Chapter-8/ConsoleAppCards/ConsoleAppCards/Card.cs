af6651c64817e72cc8ebc5dea842657a2ee2c12c
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-12 07:08:35 +0200

    program complete
    the app now shows random cards

--- Chapters/Chapter-8/ConsoleAppCards/ConsoleAppCards/Card.cs
+++ Chapters/Chapter-8/ConsoleAppCards/ConsoleAppCards/Card.cs
@@ -8,6 +8,14 @@ namespace ConsoleAppCards
 {
     internal class Card
     {
+        public Suits Suit { get; private set; }
+        public Values Value { get; private set; }
+        public string Name { get { return $"{Value} of {Suit}"; } }
 
+        public Card(Values value, Suits suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
     }
 }
--- Chapters/Chapter-8/ConsoleAppCards/ConsoleAppCards/Program.cs
+++ Chapters/Chapter-8/ConsoleAppCards/ConsoleAppCards/Program.cs
@@ -4,9 +4,23 @@ namespace ConsoleAppCards
 {
     internal class Program
     {
+        private static readonly Random random = new Random();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*int suit = random.Next(4);
+            int value = random.Next(1, 14);
+            int randomInt = random.Next();
+
+            Values values = (Values)value;
+            Suits suits = (Suits)suit;
+
+            Card card = new Card(values, suits);
+            Console.WriteLine(card.Name);*/
+
+            // All the above code can be simplified to this:
+            Card card = new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+            Console.WriteLine(card.Name);
         }
     }
 }
