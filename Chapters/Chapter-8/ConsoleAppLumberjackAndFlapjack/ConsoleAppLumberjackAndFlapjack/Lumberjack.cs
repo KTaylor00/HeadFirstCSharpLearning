0850746842a4f245ec215f309df1eb543ad49f3a
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-16 12:27:41 +0200

    create an app that helps lumberjacks eat flapjacks, using a queue and a stack

---
+++ Chapters/Chapter-8/ConsoleAppLumberjackAndFlapjack/ConsoleAppLumberjackAndFlapjack/Lumberjack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLumberjackAndFlapjack
+{
+    internal class Lumberjack
+    {
+        public string Name { get; private set; }
+        public Lumberjack(string name)
+        {
+            Name = name;
+        }
+
+        private readonly Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+
+        public void TakeFlapjack(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+
+        public void EatFlapjack()
+        {
+            Console.WriteLine($"{Name} is eating flapjacks");
+            while (flapjackStack.Count > 0)
+                Console.WriteLine($"{Name} ate a {flapjackStack.Pop().ToString().ToLower()} flapjack");
+        }
+    }
+}
