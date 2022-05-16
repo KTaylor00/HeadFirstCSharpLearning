40d37eae704e8ba5bd9b6bf255f73885d36c9ac9
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-16 09:16:54 +0200

    To show how a dictioary works

---
+++ Chapters/Chapter-8/ConsoleAppFavouriteFoodsDictionary/ConsoleAppFavouriteFoodsDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppFavouriteFoodsDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> favouriteFoods = new Dictionary<string, string>();
+            favouriteFoods["Alex"] = "hot dogs";
+            favouriteFoods["A'ja"] = "pizza";
+            favouriteFoods["Jules"] = "falafel";
+            favouriteFoods["Naima"] = "spaghetti";
+
+            string name;
+            while ((name = Console.ReadLine()) != "")
+            {
+                if (favouriteFoods.ContainsKey(name))
+                    Console.WriteLine($"{name}'s favourite food is {favouriteFoods[name]}");
+                else
+                    Console.WriteLine($"I don't know {name}'s favourite food");
+            }
+        }
+    }
+}
