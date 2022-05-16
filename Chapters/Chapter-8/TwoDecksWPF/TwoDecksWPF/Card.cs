df7a98149e9a558e27dcd3228620eb642ade36dc
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-16 13:53:20 +0200

    add the previous classes that were used in card Console apps. Add Data binding

---
+++ Chapters/Chapter-8/TwoDecksWPF/TwoDecksWPF/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDecksWPF
+{
+    internal class Card
+    {
+        public Suits Suit { get; private set; }
+        public Values Value { get; private set; }
+
+        public Card(Values value, Suits suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+
+        public override string ToString() => $"{Value} of {Suit}";
+    }
+}
