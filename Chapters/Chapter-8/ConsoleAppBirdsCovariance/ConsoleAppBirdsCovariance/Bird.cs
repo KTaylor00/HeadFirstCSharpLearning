55cb9e2ca6eda9e2d5ede66dae8b36cb1f6f73cd
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-16 09:03:39 +0200

    made this app to explain covariance

---
+++ Chapters/Chapter-8/ConsoleAppBirdsCovariance/ConsoleAppBirdsCovariance/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBirdsCovariance
+{
+    internal class Bird
+    {
+        public string Name { get; set; }
+
+        public virtual void Fly(string destination)
+        {
+            Console.WriteLine($"{this} is flying to {destination}");
+        }
+
+        public override string ToString() => $"A bird named {Name}";
+
+        public static void FlyAway(List<Bird> flock, string destination)
+        {
+            foreach (Bird bird in flock)
+            {
+                bird.Fly(destination);
+            }
+        }
+    }
+}
---
+++ Chapters/Chapter-8/ConsoleAppBirdsCovariance/ConsoleAppBirdsCovariance/Duck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBirdsCovariance
+{
+    internal class Duck : Bird
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+
+        public override string ToString() => $"{Size} inch {Kind}";
+    }
+}
