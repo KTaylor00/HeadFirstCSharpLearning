a4c9d0af58d831ee4e9a89f1e26e8a8af7398134
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-17 09:45:42 +0200

    Make a blazor version of DamageCalculator

---
+++ Chapters/Chapter-5/DamageCalculatorBlazorApp/DamageCalculatorBlazorApp/SwordDamage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageCalculatorBlazorApp
+{
+    internal class SwordDamage
+    {
+        private static Random random = new Random();
+
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        public int Damage { get; private set; }
+
+        private int roll;
+        private bool flaming;
+        private bool magic;
+
+        public int Roll { get { return roll; } set { roll = value; CalculateDamage(); } }
+
+        public bool Flaming { get { return flaming; } set { flaming = value; CalculateDamage(); } }
+
+        public bool Magic { get { return magic; } set { magic = value; CalculateDamage(); } }
+
+        public void RollDice()
+        {
+            Roll = random.Next(1, 7) * 3;
+        }
+
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic)
+                magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+
+            if (Flaming)
+                Damage += FLAME_DAMAGE;
+        }
+
+        public SwordDamage(int roll)
+        {
+            this.roll = roll;
+            CalculateDamage();
+        }
+    }
+}
--- Chapters/Chapter-8/TwoDecksWPF/TwoDecksWPF/Deck.cs
+++ Chapters/Chapter-8/TwoDecksWPF/TwoDecksWPF/Deck.cs
@@ -7,8 +7,56 @@ using System.Threading.Tasks;
 
 namespace TwoDecksWPF
 {
-    class Deck : ObservableCollection<Card>
+    internal class Deck : ObservableCollection<Card>
     {
+        private static Random random = new Random();
 
+        public Deck() => Reset();
+
+        public void Reset()
+        {
+            Clear();
+
+            for (int suit = 0; suit <= 3; suit++)
+            {
+                for (int value = 1; value <= 13; value++)
+                {
+                    Add(new Card((Values)value, (Suits)suit));
+                }
+            }
+        }
+
+        public Card Deal(int index)
+        {
+            Card cardToDeal = base[index];
+            RemoveAt(index);
+            return cardToDeal;
+        }
+
+        public void Shuffle()
+        {
+            List<Card> copy = new List<Card>(this);
+            Clear();
+            while (copy.Count > 0)
+            {
+                int index = random.Next(copy.Count);
+                Card card = copy[index];
+                copy.RemoveAt(index);
+                Add(card);
+            }
+
+
+        }
+
+        public void Sort()
+        {
+            List<Card> cards = new List<Card>(this);
+            cards.Sort(new CardComparerByValue());
+            Clear();
+            foreach (Card card in cards)
+            {
+                Add(card);
+            }
+        }
     }
 }
