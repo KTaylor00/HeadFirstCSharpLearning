abc90de87478ff174991231c85de27394e3dcb1a
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-21 07:17:35 +0200

    made a quick app to see how LINQ query works

---
+++ Chapters/Chapter-9/ConsoleAppPrizeComics/ConsoleAppPrizeComics/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleAppPrizeComics;
+
+IEnumerable<Comic> mostExpensive =
+    from comic in Comic.Catalog
+    where Comic.Prices[comic.Issue] > 500
+    orderby Comic.Prices[comic.Issue] descending
+    select comic;
+
+foreach (Comic comic in mostExpensive)
+{
+    Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
+}
