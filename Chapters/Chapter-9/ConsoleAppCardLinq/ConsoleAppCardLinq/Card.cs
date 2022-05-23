2511d9698ad8cbd6c869971b2b6858e91cbdeda2
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-23 09:01:17 +0200

    Showing LINQ grouping

---
+++ Chapters/Chapter-9/ConsoleAppCardLinq/ConsoleAppCardLinq/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCardLinq
+{
+    internal class Card : IComparable<Card>
+    {
+        public Suits Suit { get; private set; }
+        public Values Value { get; private set; }
+
+        public Card(Values value, Suits suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+
+        public override string ToString() => $"{Value} of {Suit}";
+
+        public int CompareTo(Card other)
+        {
+            return new CardComparerByValue().Compare(this, other);
+        }
+    }
+}
